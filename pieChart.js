50b9c421c94480b1d311b221fe348423569d4ced
YAO LIU <yliu0430@student.monash.edu>
2024-10-07 14:36:36 +1100

    update chart

--- pieChart.js
+++ pieChart.js
@@ -1,12 +1,46 @@
 const chartPie = {
     "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-    "description": "Pie chart showing income and expense details.",
+    "description": "Pie chart.",
+    
+    "width": 450,
+    "height": 400,
+    "background": "#585146",
     "data": {
-      "url": "https://raw.githubusercontent.com/AndyLiu010802/FIT3179-w10/main/pieChart.json",
+      "url": "https://raw.githubusercontent.com/AndyLiu010802/FIT3179/main/incomeExpense.json",
       "format": {
         "type": "json"
       }
     },
+    "params": [
+      {
+        "name": "selectedYear",
+        "value": "2020-21",
+        "bind": {
+          "input": "select",
+          "options": ["2020-21", "2021-22", "2022-23"],
+          "labels": ["2020-21", "2021-22", "2022-23"],
+          "name": "Select Year: "
+        }
+      },
+      {
+        "name": "selectedType",
+        "value": "Income",
+        "bind": {
+          "input": "select",
+          "options": ["Income", "Expense"],
+          "labels": ["Income", "Expense"],
+          "name": "Select Type: "
+        }
+      }
+    ],
+    "transform": [
+      {
+        "filter": "datum.Year == selectedYear"
+      },
+      {
+        "filter": "datum.Type == selectedType"
+      }
+    ],
     "mark": "arc",
     "encoding": {
       "theta": {"field": "Amount", "type": "quantitative"},
@@ -19,6 +53,7 @@ const chartPie = {
       ]
     }
   }
+  
   vegaEmbed('#pieChart', chartPie).then(function(result) {
   
   }).catch(console.error);
