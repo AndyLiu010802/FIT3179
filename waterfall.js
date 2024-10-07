0e0a7be13f961f0ddc29c870ecc3c458480d6d17
YAO LIU <yliu0430@student.monash.edu>
2024-10-08 00:41:53 +1100

    update labour

--- waterfall.js
+++ waterfall.js
@@ -1,5 +1,7 @@
 var waterfall = {
     "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    "width": 950,
+    "height": 650,
     "data": {
      "url": "https://raw.githubusercontent.com/AndyLiu010802/FIT3179/main/waterfall.json",
       "format": {
@@ -31,39 +33,98 @@ var waterfall = {
         "filter": "datum.Year == selectedYear && datum.Sector == selectedSector"
       }
     ],
-    "layer": [
-      {
-        "mark": {
-          "type": "bar",
-          "tooltip": true
+    "transform": [
+    {"window": [{"op": "sum", "field": "amount", "as": "sum"}]},
+    {"window": [{"op": "lead", "field": "label", "as": "lead"}]},
+    {
+      "calculate": "datum.lead === null ? datum.label : datum.lead",
+      "as": "lead"
+    },
+    {
+      "calculate": "datum.label === 'End' ? 0 : datum.sum - datum.amount",
+      "as": "previous_sum"
+    },
+    {
+      "calculate": "datum.label === 'End' ? datum.sum : datum.amount",
+      "as": "amount"
+    },
+    {
+      "calculate": "(datum.label !== 'Begin' && datum.label !== 'End' && datum.amount > 0 ? '+' : '') + datum.amount",
+      "as": "text_amount"
+    },
+    {"calculate": "(datum.sum + datum.previous_sum) / 2", "as": "center"}
+  ],
+  "encoding": {
+    "x": {
+      "field": "label",
+      "type": "ordinal",
+      "sort": null,
+      "axis": {"labelAngle": 0, "title": "Months"}
+    }
+  },
+  "layer": [
+    {
+      "mark": {"type": "bar", "size": 45},
+      "encoding": {
+        "y": {
+          "field": "previous_sum",
+          "type": "quantitative",
+          "title": "Amount"
         },
-        "encoding": {
-          "x": {
-            "field": "Sector",
-            "title": "Mining Sector",
-            "axis": {"labelAngle": 0}
-          },
-          "y": {
-            "field": "Purchases of goods and materials ($m)",
-            "title": "Selected Labor Cost"
-          },
-          "color": {"value": "#4C78A8"}
-        }
-      },
-      {
-        "mark": "rule",
-        "encoding": {
-          "y": {
-            "aggregate": "sum",
-            "field": "Industry value added ($m)",
-            "title": "Total Cost"
-          },
-          "color": {"value": "green"},
-          "size": {"value": 5}
+        "y2": {"field": "sum"},
+        "color": {
+          "condition": [
+            {
+              "test": "datum.label === 'Begin' || datum.label === 'End'",
+              "value": "#f7e0b6"
+            },
+            {"test": "datum.sum < datum.previous_sum", "value": "#f78a64"}
+          ],
+          "value": "#93c4aa"
         }
       }
-    ]
-  }
+    },
+    {
+      "mark": {
+        "type": "rule",
+        "color": "#404040",
+        "opacity": 1,
+        "strokeWidth": 2,
+        "xOffset": -22.5,
+        "x2Offset": 22.5
+      },
+      "encoding": {
+        "x2": {"field": "lead"},
+        "y": {"field": "sum", "type": "quantitative"}
+      }
+    },
+    {
+      "mark": {"type": "text", "dy": {"expr": "datum.amount >= 0 ? -4 : 4"}, "baseline": {"expr": "datum.amount >= 0 ? 'bottom' : 'top'"}},
+      "encoding": {
+        "y": {"field": "sum", "type": "quantitative"},
+        "text": {"field": "sum", "type": "nominal"}
+      }
+    },
+    {
+      "mark": {"type": "text", "fontWeight": "bold", "baseline": "middle"},
+      "encoding": {
+        "y": {"field": "center", "type": "quantitative"},
+        "text": {"field": "text_amount", "type": "nominal"},
+        "color": {
+          "condition": [
+            {
+              "test": "datum.label === 'Begin' || datum.label === 'End'",
+              "value": "#725a30"
+            }
+          ],
+          "value": "white"
+        }
+      }
+    }
+  ],
+  "config": {"text": {"fontWeight": "bold", "color": "#404040"}}
+}
+
   vegaEmbed('#waterfall', waterfall).then(function (result) {
 
   }).catch(console.error);
